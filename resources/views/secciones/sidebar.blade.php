2a927222d9d528bc8cd9e4972cb1fd84486dfe4b
andres174 <andresfer.zambrano@espam.edu.ec>
2022-07-31 14:02:17 -0500

    logo puesto

--- resources/views/secciones/sidebar.blade.php
+++ resources/views/secciones/sidebar.blade.php
@@ -2,9 +2,9 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index3.html" class="brand-link">
-        {{-- <img src="/adminlte/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
-        <i class="fa-solid fa-shop fa-lg"></i>
-        <span class="brand-text font-weight-light px-3">AdminLTE 3</span>
+        <img src="adminlte\img\logo-sistema-tienda.png" alt="AdminLTE Logo" class="brand-image elevation-3" >
+        {{-- <i class="fa-solid fa-shop fa-lg"></i> --}}
+        <span class="brand-text font-weight-light px-3">Sistema Tienda</span>
       </a>
 
     
