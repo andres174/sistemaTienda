247cc5a5226a586880a3cadfd2be483aa06875df
andres174 <andresfer.zambrano@espam.edu.ec>
2022-07-31 14:48:38 -0500

    HOME realizado

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,1 +1,27 @@
 @extends('layouts.plantilla.app')
+
+@section('header_izq')
+    BIENVENIDO A SISTEMA TIENDA
+@endsection
+
+@section('header_der')
+    
+@endsection
+
+@section('contenido')
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="\adminlte\img\carousel.png" class="d-block w-100" alt="Presentación">
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
