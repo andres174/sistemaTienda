741fbe337d05b8e29fe13022c1dbe4447f9126da
andres174 <andresfer.zambrano@espam.edu.ec>
2022-08-01 15:20:30 -0500

    cambios menores en diseño

--- resources/views/secciones/sidebar.blade.php
+++ resources/views/secciones/sidebar.blade.php
@@ -1,7 +1,7 @@
 
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="index3.html" class="brand-link">
+    <a href="{{url('/home')}}" class="brand-link">
         <img src="adminlte\img\logo-sistema-tienda.png" alt="AdminLTE Logo" class="brand-image elevation-3" >
         {{-- <i class="fa-solid fa-shop fa-lg"></i> --}}
         <span class="brand-text font-weight-light px-3">Sistema Tienda</span>
